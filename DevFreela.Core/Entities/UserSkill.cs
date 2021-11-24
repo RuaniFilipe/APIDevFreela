5341855e319b53685c73c5ea99dcaf7dc52efcbb
Ruâni Filipe de Albuquerque <71514696+RuaniFilipe@users.noreply.github.com>
2021-11-23 22:42:45 -0300

    Modelagem de Domínio - parte 1

---
+++ DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class UserSkill
+    {
+        public int IdUser { get; private set; }
+        public int IdSkill { get; private set; }
+
+        public UserSkill(int idUser, int idSkill)
+        {
+            IdUser = idUser;
+            IdSkill = idSkill;
+        }
+    }
+}
